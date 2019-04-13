2b25aff7c78e2823f85d672ed3d71aec62c5bb26
oisu <oisuoisu@gmail.com>
2019-04-14 00:57:53 +0900

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -8,17 +8,12 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>
+            実践React Native
+          </h1>
+          <h2 style={{color: 'yellow'}}>
+            ￥1,000
+          </h2>
         </header>
       </div>
     );
